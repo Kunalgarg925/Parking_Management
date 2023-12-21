b7880c68b789b372bd7b70eac912121965511640
Kunal Garg <kunalgarg925@gmail.com>
2023-12-21 10:17:38 +0530

    add test class

--- src/test/java/ParkingManagementTest.java
+++ src/test/java/ParkingManagementTest.java
@@ -1,66 +1,62 @@
 import Model.CarType;
 import Model.ParkingTicket;
 import ServiceLevel.ParkingService;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ParkingManagementTest {
     ParkingService parkingService = new ParkingService();
-
-    @BeforeClass
-    public static void setup(){
-        ParkingTicket pt1 = createTicket("Nexon", CarType.Suv);
-        ParkingTicket pt2 = createTicket("Nexon", CarType.Suv);
-        ParkingTicket pt3 = createTicket("Nexon", CarType.Suv);
-        ParkingTicket pt4 = createTicket("Nexon", CarType.Suv);
-        ParkingTicket pt5 = createTicket("Nexon", CarType.Suv);
-        ParkingTicket pt6 = createTicket("Nexon", CarType.Suv);
+    public List<String> alotSomeParkings(){
+        List<String> ticketIdList = new ArrayList<>();
+        ParkingTicket pt1 = createTicket("Nexon", CarType.ElectricCars);
+        ParkingTicket pt2 = createTicket("XUV300", CarType.Suv);
+        ParkingTicket pt3 = createTicket("Audi", CarType.SportsCars);
+        ticketIdList.add(parkingService.alotParking(pt1,2));
+        ticketIdList.add(parkingService.alotParking(pt2,3));
+        ticketIdList.add(parkingService.alotParking(pt3,2));
+        return ticketIdList;
     }
     @Test
-    public void alotParkingToCustomer(){
-
-        customer1.setTicketId(parkingService.alotParking(customer1,2));
-        parkingService.alotParking(customer2,3);
-        System.out.println("customer 1 alot ->" + customer1);
-        System.out.println("customer 2 alot ->" + customer2);
-        assertNotNull(parkingService.alotParking(customer1,2));
-        assertNotNull(parkingService.alotParking(customer2,3));
-        assertNotNull(parkingService.alotParking(customer3,5));
-        assertNotNull(parkingService.alotParking(customer4,1));
-        assertNotNull(parkingService.alotParking(customer7,8));
-        assertThrows(RuntimeException.class, () -> {parkingService.alotParking(customer6,1);});
-        assertThrows(NullPointerException.class, () -> {parkingService.alotParking(customer3,0);});
-        assertThrows(NullPointerException.class, () -> {parkingService.alotParking(customer3,null);});
-        assertThrows(NullPointerException.class, () -> {parkingService.alotParking(customer5,1);});
+    void testParkingAlotment(){
+        alotSomeParkings();
+        ParkingTicket pt1 = createTicket("BMW", CarType.LuxuryCars);
+        ParkingTicket pt2 = createTicket(null, CarType.Suv);
+        ParkingTicket pt3 = createTicket("Fortuner", CarType.HybridCars);
+        ParkingTicket pt4 = createTicket("Harrier",CarType.HybridCars);
+        ParkingTicket pt5 = createTicket("Thar", CarType.HybridCars);
+        assertNotNull(parkingService.alotParking(pt1,2));
+        assertThrows(NullPointerException.class, () -> {parkingService.alotParking(pt2,3);});
+        assertThrows(NullPointerException.class, () -> {parkingService.alotParking(pt3,0);});
+        assertNotNull(parkingService.alotParking(pt4,3));
+        System.out.println(parkingService.getAllParkingDetails().size());
+        assertThrows(RuntimeException.class, () -> {parkingService.alotParking(pt5,1);});
     }
 
     @Test
     void getAllParkingDetails(){
+        alotSomeParkings();
+        System.out.println(parkingService.getAllParkingDetails().size());
         assertNotNull(parkingService.getAllParkingDetails());
     }
 
     @Test
-    void validateParkingSlotIsNotEmpty(){
-        alotParkingToCustomer();
-        assertEquals(0,parkingService.getRemainingSlots());
+    void testRemainingParkingSlot(){
+        alotSomeParkings();
+        assertEquals(2,parkingService.getRemainingSlots());
     }
 
     @Test
     void checkOutCustomerVechile(){
-        System.out.println("checkout -> " + customer1.getTicketId());
-//        alotParkingToCustomer();
-        parkingService.checkOut(customer1.getTicketId());
-        parkingService.checkOut(customer2.getTicketId());
-        assertThrows(NullPointerException.class, () -> {parkingService.checkOut(customer6.getTicketId());});
-    }
-    @Test
-    void getRemainingSlotAfterCheckout(){
-        checkOutCustomerVechile();
-        assertEquals(2,parkingService.getRemainingSlots());
+        List<String> ticketIdList = alotSomeParkings();
+        assertDoesNotThrow(() ->{parkingService.checkOut(ticketIdList.get(0));});
+        assertThrows(NullPointerException.class, () -> {parkingService.checkOut(null);});
     }
 
-    private ParkingTicket createTicket(String carName, CarType carType) {
+    private static ParkingTicket createTicket(String carName, CarType carType) {
         ParkingTicket parkingTicket = new ParkingTicket();
         parkingTicket.setCarName(carName);
         parkingTicket.setCarType(carType);
